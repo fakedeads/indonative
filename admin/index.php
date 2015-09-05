eb3646aed93d0bcf2ace4cadbe109ffbbb15b3a4
Andika Kurniawan <dikzkeyboardfreak@gmail.com>
2015-09-05 12:01:57 +0800

    ABCD

---
+++ admin/index.php
@@ -0,0 +1,10 @@
+<?php include "header.php"; ?>
+<?php include "sidebar.php"; ?>
+
+   <div id="page-wrapper">
+   </div>
+ </div><!-- /#wrapper -->
+    <!-- Bootstrap Core JavaScript -->
+   <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ admin/sidebar.php
@@ -0,0 +1,16 @@
+
+      <div class="navbar-default sidebar" role="navigation">
+          <div class="sidebar-nav navbar-collapse">
+            <ul class="nav" id="side-menu">
+              <li>
+                <a href="#"><i class="fa fa-envelope nav_icon"></i>Articles</a>
+              </li>
+              <li>
+                <a href="#"><i class="fa fa-envelope nav_icon"></i>Tutorials</a>
+              </li>
+            </ul>
+          </div>
+          <!-- /.sidebar-collapse -->
+      </div>
+      <!-- /.navbar-static-side -->
+    </nav>
