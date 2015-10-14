705087d7e653c2fb3f4cfa1cec7c088f68f68bba
wdn93 <w.d.nurhalim@gmail.com>
2015-10-14 19:16:05 +0700

    nambahin tiny mce

--- admin/articles.php
+++ admin/articles.php
@@ -2,30 +2,89 @@
   include "header.php";
   include "sidebar.php";
 ?>
-<div id="page-wrapper">
-			<form action="admin_controller.php" method="post" enctype="multipart/form-data">
-				<input type="hidden" name="code" value="articles">
-					<div class="register-top-grid">
-						<h3>Add Articles</h3>
-						<div class="wow fadeInRight col-md-8 col-sm-12" data-wow-delay="0.4s">
-							<span>Title<label>*</label></span>
-							<input type="text" name="title">
-						</div>
-						<div class="wow fadeInRight col-md-8 col-sm-12" data-wow-delay="0.4s">
-							<span>Description<label>*</label></span>
-							<textarea id="description" style="padding:0px; height:150px" name="description">
-							</textarea>
-						</div>
-						<div class="wow fadeInRight col-md-8 col-sm-12" data-wow-delay="0.4s">
-							<span>Photo<label>*</label></span>
-							<input type="file" name="photo" size="25" />
-						</div>
-						<div class="wow fadeInRight col-md-8 col-sm-12" data-wow-delay="0.4s">
-							<input type="submit" value="submit">
+<script type="text/javascript" src="tinymce/jscripts/tiny_mce/tiny_mce.js"></script>
+<script type="text/javascript">
+tinyMCE.init({
+        // General options
+        mode : "textareas",
+        theme : "advanced",
+        plugins : "autolink,lists,spellchecker,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template",
+
+        // Theme options
+        theme_advanced_buttons1 : "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,fontselect,fontsizeselect",
+        theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
+        theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
+        theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,spellchecker,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,blockquote,pagebreak,|,insertfile,insertimage",
+        theme_advanced_toolbar_location : "top",
+        theme_advanced_toolbar_align : "left",
+        theme_advanced_statusbar_location : "bottom",
+        theme_advanced_resizing : true,
+
+        // Skin options
+        skin : "o2k7",
+        skin_variant : "black",
+
+        // Example content CSS (should be your site CSS)
+        content_css : "css/example.css",
+
+        // Drop lists for link/image/media/template dialogs
+        template_external_list_url : "js/template_list.js",
+        external_link_list_url : "js/link_list.js",
+        external_image_list_url : "js/image_list.js",
+        media_external_list_url : "js/media_list.js",
+
+        // Replace values for the template plugin
+        template_replace_values : {
+                username : "Some User",
+                staffid : "991234"
+        }
+});
+</script>
+	<script type="text/javascript" src="js/jquery-1.7.1.min.js"></script>
+    <script type="text/javascript" src="js/jquery-ui-1.8.17.custom.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" />
+ 
+  <div id="page-wrapper">
+		<div class="graphs">
+			 <div class="xs">
+				 	<h3>Add Articles</h3>
+					<div class="tab-content">
+						<div class="tab-pane active" id="horizontal-form">
+							<form class="form-horizontal" action="admin_controller.php" method="post" enctype="multipart/form-data">
+								<input type="hidden" name="code" value="articles">
+								<div class="form-group">
+									<label for="disabledinput" class="col-sm-2 control-label">Title*</label>
+									<div class="col-sm-8">
+										<input type="text" class="form-control1" id="focusedinput" placeholder="Default Input" name="title">
+									</div>
+								</div>
+								<div class="form-group">
+									<label for="disabledinput" class="col-sm-2 control-label">Description*</label>
+									<div class="col-sm-8">
+										<textarea id="description" cols="0" rows="0" class="form-control1" name="description" style="height: 200px;">
+										</textarea>
+									</div>
+								</div>
+								<div class="form-group">
+					        <label for="exampleInputFile" class="col-sm-2 control-label">Photo*</label>
+									<div class="col-sm-8">
+						        <input type="file" name="photo" size="25" id="exampleInputFile">
+						        <p class="help-block">Example block-level help text here.</p>
+									</div>
+					      </div>
+								<div class="panel-footer">
+									<div class="row">
+										<div class="col-sm-8 col-sm-offset-2">
+											<input type="submit" value="submit" class="btn-success btn">
+										</div>
+									</div>
+							 </div>
+							</form>
 						</div>
 					</div>
-			</form>
-</div>
+			</div>
+		</div>
+	</div>
 <?php
   include "footer.php";
 ?>
